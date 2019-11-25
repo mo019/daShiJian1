406ba449a3f90585d439291065b41d59b83042dd
unknown <you@example.com>
2019-11-25 20:20:18 +0800

    用箭头函数和let优化一下代码

--- web_back/js/article_categort.js
+++ web_back/js/article_categort.js
@@ -3,13 +3,33 @@ $(function(){
     // 从后端取出数据放到页面上
 jiceng.wenZhangChaXun(function(res){
 
-    var htmlStr =template('moban',res)
+    let htmlStr =template('moban',res)
     $('#wenZhangGuanLi').html(htmlStr);
 
 });
-// var htmlStr = template('moban',res)
-// console.log(htmlStr)
-// $('#wenZhangGuanLi').html(htmlStr);
+
+
+$('#addModal').modal({
+    show:false,
+    backdrop:false
+  });
+
+  $('#model_shutoff').click(function(){
+    alert('shutoff');
+    $('#addModal').modal('hide');
+  });
+
+  $('#model_add').click(function(){
+    alert('add');
+    $('#addModal').modal('hide');
+  });
+
+  $('.category_table').delegate('a','click',() => {
+      if($(this).hasClass('btn-info')){
+        $('#addModal .modal-title').html('修改分类名称');
+        $('#addModal').modal('show');
+      }
+  })
 
 
 
--- web_back/js/base.js
+++ web_back/js/base.js
@@ -1,46 +1,28 @@
 
-var dizhi = 'http://localhost:8000';
+let dizhi = 'http://localhost:8000';
 
 // 账号登陆接口
-var jiben_denglu = 'http://localhost:8000/admin/login';
+let jiben_denglu = 'http://localhost:8000/admin/login';
 // 显示用户头像名字等信息接口
-var jiben_xianshi = 'http://localhost:8000/admin/getuser';
+let jiben_xianshi = 'http://localhost:8000/admin/getuser';
 // 登出接口
-var jiben_tuichu = 'http://localhost:8000/admin/logout';
+let jiben_tuichu = 'http://localhost:8000/admin/logout';
 // 文章类别查询
-var jiben_wenZhangChaXun = 'http://localhost:8000/admin/category_search'
+let jiben_wenZhangChaXun = 'http://localhost:8000/admin/category_search'
 
 
 //  jiceng就是接口调用封装的对象
-var jiceng = {
-
-
+let jiceng = {
     // 账号登陆接口
-    denglu: function (zhanghao, mima, login_huidiao) {
-        $.post(jiben_denglu,
-            { user_name: zhanghao, password: mima },
-            function (res) {
-                login_huidiao(res)
-            })
+    denglu: (zhanghao, mima, login_huidiao) => { 
+        $.post(jiben_denglu, { user_name: zhanghao, password: mima }, (res) => login_huidiao(res)) 
     },
     //    显示用户头像名字等信息接口
-    xianshi: function (index_huidiao) {
-        $.get(jiben_xianshi, function (res) {
-            index_huidiao(res);
-        })
-    },
+    xianshi: index_huidiao => $.get(jiben_xianshi, res => index_huidiao(res)),
     // 登出接口
-    tuichu: function (tanchu) {
-        $.post(jiben_tuichu, function (res) {
-            tanchu(res);
-        })
-    },
+    tuichu: tanchu => $.post(jiben_tuichu, res => tanchu(res)),
     // 文章类别查询
-    wenZhangChaXun: function (chaXun) {
-        $.get(jiben_wenZhangChaXun, function (res) {
-            chaXun(res);
-        })
-    }
+    wenZhangChaXun: chaXun => $.get(jiben_wenZhangChaXun, res => chaXun(res))
 
 
 
